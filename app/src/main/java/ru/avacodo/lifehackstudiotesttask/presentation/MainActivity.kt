5e960fb38d0d92398bd00276a762a6523f5538e6
Dmitry Gusev <dmtrxiii@gmail.com>
2022-11-29 16:34:24 +0300

    navigation fragment released

--- app/src/main/java/ru/avacodo/lifehackstudiotesttask/presentation/MainActivity.kt
+++ app/src/main/java/ru/avacodo/lifehackstudiotesttask/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.avacodo.lifehackstudiotesttask.R
 import ru.avacodo.lifehackstudiotesttask.presentation.core.NavigationFragmentHolder
+import ru.avacodo.lifehackstudiotesttask.presentation.navigation.NavigationFragment
 
 private const val NAVIGATION_FRAGMENT_TAG = "ROUTER"
 
@@ -11,6 +12,13 @@ class MainActivity : AppCompatActivity(), NavigationFragmentHolder {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_container, NavigationFragment(), NAVIGATION_FRAGMENT_TAG)
+                .commit()
+        }
     }
 
     override fun getNavigationFragmentTag(): String {
--- app/src/main/java/ru/avacodo/lifehackstudiotesttask/presentation/navigation/NavigationFragment.kt
+++ app/src/main/java/ru/avacodo/lifehackstudiotesttask/presentation/navigation/NavigationFragment.kt
@@ -1,8 +1,46 @@
 package ru.avacodo.lifehackstudiotesttask.presentation.navigation
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ru.avacodo.lifehackstudiotesttask.R
 import ru.avacodo.lifehackstudiotesttask.databinding.FragmentNavigationBinding
+import ru.avacodo.lifehackstudiotesttask.presentation.core.AppScreensNavigator
 import ru.avacodo.lifehackstudiotesttask.presentation.core.BaseFragment
+import ru.avacodo.lifehackstudiotesttask.presentation.screens.details.CompanyDetailsFragment
+import ru.avacodo.lifehackstudiotesttask.presentation.screens.list.CompaniesListFragment
+
+private const val DEFAULT_BACKSTACK_NAME = "DEFAULT_BACKSTACK"
 
 class NavigationFragment :
-    BaseFragment<FragmentNavigationBinding>(FragmentNavigationBinding::inflate) {
+    BaseFragment<FragmentNavigationBinding>(FragmentNavigationBinding::inflate),
+    AppScreensNavigator {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (savedInstanceState == null) {
+            showCompaniesListFragment()
+        }
+    }
+
+    override fun showCompaniesListFragment() {
+        navigateToDestination(CompaniesListFragment())
+    }
+
+    override fun showCompanyDetailsFragment(companyId: String) {
+        navigateToDestination(CompanyDetailsFragment.newInstance(companyId), true)
+    }
+
+    private fun navigateToDestination(
+        destination: Fragment,
+        isRequiredToAddToBackStack: Boolean = false,
+    ) {
+        parentFragmentManager
+            .beginTransaction().apply {
+                replace(R.id.navigation_container, destination)
+                if (isRequiredToAddToBackStack) {
+                    addToBackStack(DEFAULT_BACKSTACK_NAME)
+                }
+            }.commit()
+    }
 }
