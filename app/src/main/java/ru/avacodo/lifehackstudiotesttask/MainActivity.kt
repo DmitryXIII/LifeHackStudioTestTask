18b0c954f0f8d00526a40b955a36c9299bc940d3
Dmitry Gusev <dmtrxiii@gmail.com>
2022-11-29 12:05:14 +0300

    Initial commit

---
+++ app/src/main/java/ru/avacodo/lifehackstudiotesttask/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.avacodo.lifehackstudiotesttask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
