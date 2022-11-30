33427b78e50ad31815ac23ffd12874558cbbe3fb
Dmitry Gusev <dmtrxiii@gmail.com>
2022-11-30 14:42:53 +0300

    comments added

--- app/src/main/java/ru/avacodo/lifehackstudiotesttask/presentation/MainActivity.kt
+++ app/src/main/java/ru/avacodo/lifehackstudiotesttask/presentation/MainActivity.kt
@@ -9,6 +9,20 @@ import ru.avacodo.lifehackstudiotesttask.presentation.core.NavigationFragmentHol
 import ru.avacodo.lifehackstudiotesttask.presentation.core.SplashScreenKeeper
 import ru.avacodo.lifehackstudiotesttask.presentation.navigation.NavigationFragment
 
+/**
+ * created by Avacodo 30.11.2022
+ * 1) Ошибку, возникающую при получении данных о компаниях 6 и 7, видел -
+ * в исходном json'е в объекте description прилетает лишняя кавычка,
+ * которая крашит конвертацию из json.
+ * Сходу не придумал, как в ответе сервера эту кавычку удалить, поэтому ошибка поймана и
+ * (для экономии времени на отправку задания) обработана как ошибка на стороне сервера.
+ * Но буду разбираться - кейс интересный, как в рантайме перед началом конвертации заменять символы
+ * в ответе сервера.
+ *
+ * 2) Для корректного отображения splash screen приложение необходимо запускать с утсройства
+ * (при запуске из студии гугловый сплэш показывает только фон, иконка не отображается)
+ */
+
 private const val NAVIGATION_FRAGMENT_TAG = "ROUTER"
 
 class MainActivity : AppCompatActivity(), NavigationFragmentHolder, SplashScreenKeeper {
